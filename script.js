5687f75e6dd53aa2dfb8d951eba79718e718efc7
ihor97 <daniel.waffner@gmail.com>
2023-07-01 12:40:24 +0100

    liskov substitution principle

--- script.js
+++ script.js
@@ -1,100 +1,67 @@
-// enterprise design pattern
-
-
-let Color=Object.freeze({
-    red:'red',
-    green:'green',
-    blue:'blue'
-})
-let Size=Object.freeze({
-    small:'small',
-    medium:'medium',
-    large:'large'
-})
-
-class Product{
-    constructor(name,color,size){
-        this.name=name
-        this.color=color
-        this.size=size
+// якщо в тебе функція яка бере базовий клас то во ця ф-я мусить брати теж клас наслідник його 
+class Recatangle{
+    constructor(width,height){
+        this._width=width
+        this._height=height
     }
+
+    get area(){
+        return this._width*this._height
+    }
+    toString(){
+        return `${this._height}*${this._width}`
+    }
+    get width(){
+        return this._width
+    }
+    get height(){
+        return this._height
+    }
+    set width(val){
+         this._width=val
+    }
+    set height(val){
+         this._height=val
+    }
+    
+    
 }
-// open closed principle - обєкти відкриті для розширення але закриті для модифікації
-// під extension розуміється що клас наслідує властивості вже 
-// тобто ми не ліхемо у існуючі класи і не модуфікуємо їх
-class ProductFilter{
-    filterByColor(products,color){
-        // якщо ми будемо тут щось міняти тоді це буде модіфікація і це погано 
-        return products.filter(p=>p.color===color)
-    }
-    filterBySize(products,size){
-        return products.filter(p=>p.size===size)
-
-    }
-    filterBySizeAndColor(products,size,color){
-        return products.filter(p=>p.size===size&&p.color===color)
-    }
-    // state space explosion - тобто ми не можемо безкінечно аихати сюди різні методи
-}
-
-
-// specification -клас що відповідає за фільтрування
-
-class ColorSpecification{
-    constructor(color){
-        this.color=color
-    }
-    isStisfied(item){
-        return item.color===this.color
-    }
-}
-class SizeSpecification{
+class Square extends Recatangle{
     constructor(size){
-        this.size=size
+        super(size,size)
     }
-    isStisfied(item){
-        return item.size===this.size
+    set width(val){
+        this._width=this._height=val
     }
-}
-// комбінатори
-class AndSpecification{
-    constructor(...specs){
-        this.specs=specs
-    }
-    isStisfied(item){
-        return this.specs.every(x=>x.isStisfied(item))
-    }
-}
-let apple=new Product('apple',Color.green,Size.small)
-let tree=new Product('tree',Color.green,Size.large)
-let house=new Product('house',Color.blue,Size.large)
-let products=[apple,tree,house]
-// let pf= new ProductFilter()
-// console.log(`green products(old)`);
-// for (const p of pf.filterByColor(products,Color.green)) {
-//     console.log(`* ${p.name} is green`);
-// }
+    set height(val){
+        this._width=this._height=val
 
-class BetterFilter{
-    filter(items,spec){
-        return items.filter(x=>spec.isStisfied(x))
     }
 }
+// функція буде ламатися 
+let useIt=function (rec) {
+    let width=rec._width
+    rec.height=10
+    console.log(
+        `expected area of ${10*width}
+        got ${rec.area}`
+    );
 
-let bF=new BetterFilter()
-console.log( 'greeen products (new)');
-for (const p of bF.filter(products,new ColorSpecification(Color.green))) {
-    console.log(`* ${p.name} is green`);
+
 }
 
 
-console.log(`large and green`);
+let rec=new Recatangle(2,3)
+useIt(rec)
 
-let spec = new AndSpecification(
-    new ColorSpecification(Color.green),
-    new SizeSpecification(Size.large)
-)
 
-for (const p of bF.filter(products,spec)) {
-    console.log(`* ${p.name} is green and large`);
-}
+let sq=new Square(5)
+useIt(sq)
+
+console.log(sq.toString());
+sq.width=10
+console.log(sq.toString());
+
+
+
+
