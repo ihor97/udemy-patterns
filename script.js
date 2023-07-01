ef2c39d665059ab3284a99af9a3f29bd66974443
ihor97 <daniel.waffner@gmail.com>
2023-07-01 12:20:38 +0100

    open closed principle

--- script.js
+++ script.js
@@ -1,52 +1,100 @@
+// enterprise design pattern
 
-const fs=require('fs')
-class Journal{
-    constructor(){
-        this.entries={}
-    }
-    addEntry(text){
-        let c=++Journal.count
-        let entry=`${c}:${text}`
-        this.entries[c]=entry
-        return c
-    }
-    removeEntry(index){
-        delete this.entries[index]
-    }
-    toString(){
-        return Object.values(this.entries).join('\n')
-    }
 
-}
+let Color=Object.freeze({
+    red:'red',
+    green:'green',
+    blue:'blue'
+})
+let Size=Object.freeze({
+    small:'small',
+    medium:'medium',
+    large:'large'
+})
 
-// це вже друга якась відповідальність
-class PersistenceManager{
-    preprocess(j){
-
-    }
-    saveToFile(journal,filename){
-        // створює новий файл
-        fs.writeFileSync(filename,journal.toString())
-    }
-    load(filename){
-        /////
-    }
-    loadFromUrl(url){
-        /////
+class Product{
+    constructor(name,color,size){
+        this.name=name
+        this.color=color
+        this.size=size
     }
 }
+// open closed principle - обєкти відкриті для розширення але закриті для модифікації
+// під extension розуміється що клас наслідує властивості вже 
+// тобто ми не ліхемо у існуючі класи і не модуфікуємо їх
+class ProductFilter{
+    filterByColor(products,color){
+        // якщо ми будемо тут щось міняти тоді це буде модіфікація і це погано 
+        return products.filter(p=>p.color===color)
+    }
+    filterBySize(products,size){
+        return products.filter(p=>p.size===size)
+
+    }
+    filterBySizeAndColor(products,size,color){
+        return products.filter(p=>p.size===size&&p.color===color)
+    }
+    // state space explosion - тобто ми не можемо безкінечно аихати сюди різні методи
+}
 
 
-Journal.count=0
+// specification -клас що відповідає за фільтрування
+
+class ColorSpecification{
+    constructor(color){
+        this.color=color
+    }
+    isStisfied(item){
+        return item.color===this.color
+    }
+}
+class SizeSpecification{
+    constructor(size){
+        this.size=size
+    }
+    isStisfied(item){
+        return item.size===this.size
+    }
+}
+// комбінатори
+class AndSpecification{
+    constructor(...specs){
+        this.specs=specs
+    }
+    isStisfied(item){
+        return this.specs.every(x=>x.isStisfied(item))
+    }
+}
+let apple=new Product('apple',Color.green,Size.small)
+let tree=new Product('tree',Color.green,Size.large)
+let house=new Product('house',Color.blue,Size.large)
+let products=[apple,tree,house]
+// let pf= new ProductFilter()
+// console.log(`green products(old)`);
+// for (const p of pf.filterByColor(products,Color.green)) {
+//     console.log(`* ${p.name} is green`);
+// }
+
+class BetterFilter{
+    filter(items,spec){
+        return items.filter(x=>spec.isStisfied(x))
+    }
+}
+
+let bF=new BetterFilter()
+console.log( 'greeen products (new)');
+for (const p of bF.filter(products,new ColorSpecification(Color.green))) {
+    console.log(`* ${p.name} is green`);
+}
 
 
-let j=new Journal()
+console.log(`large and green`);
 
-j.addEntry('one')
-j.addEntry('two')
-console.log(j.toString());
+let spec = new AndSpecification(
+    new ColorSpecification(Color.green),
+    new SizeSpecification(Size.large)
+)
 
-let p=new PersistenceManager()
-let filename='journal.txt'
-
-p.saveToFile(j,filename)
+for (const p of bF.filter(products,spec)) {
+    console.log(`* ${p.name} is green and large`);
+}
